243244af9253814c631ac1140e185c77b04eae8e
mdaeron <mathieu@daeron.fr>
2020-12-10 12:22:30 +0100

    Update version

--- D47crunch/__init__.py
+++ D47crunch/__init__.py
@@ -13,8 +13,8 @@ __author__    = 'Mathieu Daëron'
 __contact__   = 'daeron@lsce.ipsl.fr'
 __copyright__ = 'Copyright (c) 2020 Mathieu Daëron'
 __license__   = 'Modified BSD License - https://opensource.org/licenses/BSD-3-Clause'
-__date__      = '2020-09-30'
-__version__   = '0.6.2dev1'
+__date__      = '2020-12-10'
+__version__   = '1.0.0'
 
 import os
 import numpy as np
